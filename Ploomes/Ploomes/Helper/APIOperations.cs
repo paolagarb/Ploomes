052ad86ffb12745ef5b45678ecda697f89196332
Paola Garbato <paolagarbato@gmail.com>
2021-01-17 13:58:49 -0300

    Tratamento de exceções e otimização do código.

--- Ploomes/Ploomes/Helper/APIOperations.cs
+++ Ploomes/Ploomes/Helper/APIOperations.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Ploomes.Entities;
+using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,115 +13,86 @@ namespace Ploomes.Helper
 
         public async Task<object> CreateContact(Contacts contacts)
         {
-            HttpClient client = _api.Initial();
-
-            var jsonContent = JsonConvert.SerializeObject(contacts);
-            var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-            var response = await client.PostAsync("/Contacts?$select=Id", contentString);
-            if (response.IsSuccessStatusCode)
-            {
-                var res = response.Content.ReadAsStringAsync().Result;
-                return JsonConvert.DeserializeObject(res);
-            }
-            return null;
+            return Post("/Contacts?$select=Id", contacts, "criar o cliente.");
         }
 
         public async Task<object> CreateDeals(Deals deals)
         {
-            HttpClient client = _api.Initial();
-
-            var jsonContent = JsonConvert.SerializeObject(deals);
-            var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-            var response = await client.PostAsync("/Deals?$select=Id", contentString);
-            if (response.IsSuccessStatusCode)
-            {
-                var res = response.Content.ReadAsStringAsync().Result;
-                return JsonConvert.DeserializeObject(res);
-            }
-            return null;
+            return Post("/Deals?$select=Id", deals, "criar a negociação.");
         }
 
         public async Task<object> CreateTasks(Tasks tasks)
         {
-            HttpClient client = _api.Initial();
-
-            var jsonContent = JsonConvert.SerializeObject(tasks);
-            var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-            var response = await client.PostAsync("/Tasks?#select=Id", contentString);
-            if (response.IsSuccessStatusCode)
-            {
-                var res = response.Content.ReadAsStringAsync().Result;
-                return JsonConvert.DeserializeObject(res);
-
-            }
-            return null;
+            return Post("/Tasks?#select=Id", tasks, "criar a tarefa.");
         }
 
         public async Task<object> UpdateDeals(Deals dealsUpdate, int id)
         {
-            HttpClient client = _api.Initial();
-
-            var jsonContent = JsonConvert.SerializeObject(dealsUpdate);
-            var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-            var response = await client.PatchAsync($"/Deals({id})?#select=Id", contentString);
-            if (response.IsSuccessStatusCode)
-            {
-                var res = response.Content.ReadAsStringAsync().Result;
-                return JsonConvert.DeserializeObject(res);
-            }
-            return null;
+            return Patch($"/Deals({id})?#select=Id", dealsUpdate, "atualizar a negociação.");
         }
 
         public async Task<object> FinishTasks(Tasks tasks, int id)
         {
-            HttpClient client = _api.Initial();
-
-            var jsonContent = JsonConvert.SerializeObject(tasks);
-            var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-            var response = await client.PostAsync($"/Tasks({id})/Finish?#select=Id", contentString);
-            if (response.IsSuccessStatusCode)
-            {
-                var res = response.Content.ReadAsStringAsync().Result;
-                return JsonConvert.DeserializeObject(res);
-            }
-            return null;
+            return Post($"/Tasks({id})/Finish?#select=Id", tasks, "finalizar a tarefa.");
         }
+
         public async Task<object> WinDeals(Deals deals, int id)
         {
-            HttpClient client = _api.Initial();
-
-            var jsonContent = JsonConvert.SerializeObject(deals);
-            var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-            var response = await client.PostAsync($"/Deals({id})/Win?$select=Id", contentString);
-            if (response.IsSuccessStatusCode)
-            {
-                var res = response.Content.ReadAsStringAsync().Result;
-                return JsonConvert.DeserializeObject(res);
-            }
-            return null;
+            return Post($"/Deals({id})/Win?$select=Id", deals, "registrar a negociação");
         }
 
         public async Task<object> InteractionRecords(InteractionsRecords interactionsRecords)
         {
-            HttpClient client = _api.Initial();
+            return Post("/InteractionsRecords?$select=Id", interactionsRecords, "escrever no histórico.");
+        }
 
-            var jsonContent = JsonConvert.SerializeObject(interactionsRecords);
-            var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-            var response = await client.PostAsync("/InteractionsRecords?$select=Id", contentString);
-            if (response.IsSuccessStatusCode)
+        public async Task<object> Patch(string requestUri, object value, string message)
+        {
+            try
             {
-                var res = response.Content.ReadAsStringAsync().Result;
-                return JsonConvert.DeserializeObject(res);
+                HttpClient client = _api.Initial();
 
+                var jsonContent = JsonConvert.SerializeObject(value);
+                var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                var response = await client.PatchAsync(requestUri, contentString);
+                if (response.IsSuccessStatusCode)
+                {
+                    var res = response.Content.ReadAsStringAsync().Result;
+                    return JsonConvert.DeserializeObject(res);
+                }
+                return null;
             }
-            return null;
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ocorreu um erro ao {message}");
+                return null;
+            }
+        }
+
+        public async Task<object> Post(string requestUri, object value, string message)
+        {
+            try
+            {
+                HttpClient client = _api.Initial();
+
+                var jsonContent = JsonConvert.SerializeObject(value);
+                var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(requestUri, contentString);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var res = response.Content.ReadAsStringAsync().Result;
+                    return JsonConvert.DeserializeObject(res);
+                }
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ocorreu um erro ao {message}");
+                return null;
+            }
         }
     }
 }
